80f93165174dff1759583f49dfe0c30a93ca103c
Niclas <niclas.persson01@gmail.com>
2023-04-27 13:24:36 +0200

    Changed function names to match instruction (dumb)

--- Assignment 2/2.3/Assignment23.py
+++ Assignment 2/2.3/Assignment23.py
@@ -10,12 +10,12 @@ from tqdm.auto import tqdm
 # Change these parameters as desired
 filename = "data_ga.txt"
 populationSize = 500  # Needs to be even
-numberOfGenerations = 2000
-tournamentProbability = 0.65
+numberOfGenerations = 5000
+tournamentProbability = 0.8
 crossoverProbability = 0.2
-mutationProbability = 0.3
+mutationProbability = 0.4
 creepProbability = 0.8
-creepRate = 0.03
+creepRate = 0.05
 
 # Do not change these
 numberOfParameters = 6
@@ -54,7 +54,7 @@ def calc_fitness(error):
 #######################
 
 
-def initialize_population(populationSize=100, numberOfParameters=6, empty=False):
+def InitializePopulation(populationSize=100, numberOfParameters=6, empty=False):
     if not empty:
         pop = np.random.uniform(-maximumParameterValue, maximumParameterValue + 0.00001, size=(populationSize, numberOfParameters))
     else:
@@ -62,7 +62,7 @@ def initialize_population(populationSize=100, numberOfParameters=6, empty=False)
     return pop
 
 
-def evaluate_individual(chromosome, functionData):
+def EvaluateIndividual(chromosome, functionData):
     alldata = functionData
     approx = g_fun(chromosome, alldata[0], alldata[1])
     error = calc_error(approx, alldata[2])
@@ -70,7 +70,7 @@ def evaluate_individual(chromosome, functionData):
     return fitness
 
 
-def tournament_select(fitness, tournamentProbability, size=2):
+def TournamentSelect(fitness, tournamentProbability, size=2):
     tp = tournamentProbability
     random_1 = random.randint(0, len(fitness) - 1)
     random_2 = random.randint(0, len(fitness) - 1)
@@ -95,7 +95,7 @@ def tournament_select(fitness, tournamentProbability, size=2):
     return ind
 
 
-def cross(chromosome_1, chromosome_2, p_cross):
+def Cross(chromosome_1, chromosome_2, p_cross):
     if random.random() > p_cross:
         return chromosome_1, chromosome_2
     else:
@@ -105,7 +105,7 @@ def cross(chromosome_1, chromosome_2, p_cross):
         return new_ind_1, new_ind_2
 
 
-def mutate(originalchromosome, mutationprobability, creepprobability, creeprate):
+def Mutate(originalchromosome, mutationprobability, creepprobability, creeprate):
     newchromosome = []
     for gene in originalchromosome:
         # Do not perform any mutation
@@ -128,7 +128,7 @@ def main():
 
     # Initialize everything
     xdata, ydata, gdata, alldata = importdata(filename)
-    population = initialize_population()
+    population = InitializePopulation()
     igeneration = 0
     maximum_fitness = []
     best_chromosome = None
@@ -146,7 +146,7 @@ def main():
 
         # Evaluate generation
         for i in range(len(population)):
-            f_ind = evaluate_individual(population[i], alldata)
+            f_ind = EvaluateIndividual(population[i], alldata)
             fitness_population.append(f_ind)
             if f_ind > fitness:
                 fitness = f_ind
@@ -158,13 +158,13 @@ def main():
         best_chromosome = population[best_index]
 
         # Generate temporary population
-        temp_pop = initialize_population(populationSize, empty=True)
+        temp_pop = InitializePopulation(populationSize, empty=True)
 
         # Transfer from old population to temporary
         for m in range(0, populationSize, 2):
             # Select two individuals based tournament selection
-            ind_1 = tournament_select(fitness_population, tournamentProbability)
-            ind_2 = tournament_select(fitness_population, tournamentProbability)
+            ind_1 = TournamentSelect(fitness_population, tournamentProbability)
+            ind_2 = TournamentSelect(fitness_population, tournamentProbability)
 
             # Remove them from population
             np.delete(fitness_population, ind_1)
@@ -175,14 +175,14 @@ def main():
             chromosome_2 = population[ind_2]
 
             # Perform crossover
-            new_individual_pair = cross(chromosome_1, chromosome_2, crossoverProbability)
+            new_individual_pair = Cross(chromosome_1, chromosome_2, crossoverProbability)
             temp_pop[m] = new_individual_pair[0]
             temp_pop[m + 1] = new_individual_pair[1]
 
         # Mutate temp poulation
         for n in range(1, populationSize):
             original_chromosome = temp_pop[n]
-            mutated_chromosome = mutate(original_chromosome, mutationProbability, creepProbability, creepRate)
+            mutated_chromosome = Mutate(original_chromosome, mutationProbability, creepProbability, creepRate)
             temp_pop[n] = mutated_chromosome
 
         temp_pop[0] = best_chromosome  # Elitism
@@ -195,7 +195,8 @@ def main():
     with open("results.txt", "a") as file:
         file.write(f"Fitness: {fitness}, Pop_size: {populationSize}, Generations: {numberOfGenerations}\n"
                    f"TP: {tournamentProbability}, COP: {crossoverProbability} MP: {mutationProbability},"
-                   f"CP: {creepProbability}, CR: {creepRate}\n \n")
+                   f"CP: {creepProbability}, CR: {creepRate}\n"
+                   f"Chromosome: {best_chromosome} \n \n")
 
     plt.figure()
     plt.title("GA Performance over generation")
